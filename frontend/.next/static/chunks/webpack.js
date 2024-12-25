611c78fb4d8f89f42c7911c3cafa09558966dbb8
yamatovision <shiraishi.tatsuya@mikoto.co.jp>
2024-12-25 16:08:18 +0900

    微細なエラー修正

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45b5cdbce66a8af5"; }
+/******/ 		__webpack_require__.h = function() { return "4795df1a95133bd9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/src/lib/api/cloudinaryUpload.ts
+++ frontend/src/lib/api/cloudinaryUpload.ts
@@ -1,11 +1,3 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 export async function uploadToCloudinary(file: File, folder: string = 'courses'): Promise<string> {
   const formData = new FormData();
   formData.append('file', file);
